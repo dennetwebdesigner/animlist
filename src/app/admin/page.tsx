1a065c6cf1dfad1dc259695022cb8d20cf08245c
dennetwebdesigner <dennetwebdesigner@gmail.com>
2023-11-01 11:45:18 -0300

    dashboard admin incomplet

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,23 @@
-'use client'
-import { useEffect, useState } from "react"
+"use client";
+import { AdminSystemValidate } from "@/functions/Auth/AdminSystem";
+import { get_user_info } from "@/functions/Auth/googleProvider";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default  function(){
-    const [user, setUser] = useState<any>(null)
+export default function () {
+  const [user, setUser] = useState<any>(null);
+  const [role, setRole] = useState<any>(null);
+  const router = useRouter();
+  useEffect(() => {
+    if (!user) {
+      get_user_info(setUser);
+    } else if (user && !role) {
+      AdminSystemValidate(user?.uid).then((r) => {
+        if (r) setRole(r);
+        else router.push("/");
+      });
+    }
+  }, [user, role]);
 
-    useEffect(() => {
-        
-    }, [])
-
-    return (
-        <>
-            <h1>Ala Administrativa</h1>
-        </>
-    )
+  return <>{role && <h2>Ala administrativa</h2>}</>;
 }
